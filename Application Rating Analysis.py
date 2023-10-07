e1e8e91fc5eb60870a03546a98a36bc3c527d010
Oleksandr-Dykyi <144490786+Oleksandr-Dykyi@users.noreply.github.com>
2023-10-07 17:04:05 +0300

    Add files via upload

--- Application Rating Analysis.py
+++ Application Rating Analysis.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 # In[1]:
 
 
@@ -91,10 +88,3 @@ plt.xlabel('Week')
 plt.ylabel('Average statement rating')
 plt.ylim(0, 55)
 plt.show()
-
-
-# In[ ]:
-
-
-
-
